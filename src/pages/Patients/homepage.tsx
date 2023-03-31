19d30ecf96bd9622ccf9e98480b1179fe4c898b0
ProMahabub21 <imahbub.1999@gmail.com>
2023-04-01 05:05:36 +0600

    initial setup with tailwind

---
+++ src/pages/Patients/homepage.tsx
@@ -0,0 +1,7 @@
+// generate boilerplate
+import React from "react";
+
+export default function HomePage() {
+
+    return <h1>This is Patients Homepage.</h1>
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,22 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      {/* <div className="grid grid-cols-12 gap-6 mt-5">
+        <div className="col-span-12 text-center bg-green-400 lg:col-span-3 rounded-2xl dark:bg-dark-500 dark:shadow-custom-dark ">
+          <h1 className="bg-dark-500">SideBar</h1>
+        </div>
+        <div className="col-span-12 text-center bg-green-400 lg:col-span-9 rounded-2xl dark:bg-dark-500 dark:shadow-custom-dark ">
+          <h1 className="bg-dark-500">Main Body</h1>
+        </div>
+
+      </div> */}
+
+
+      <Component {...pageProps} />
+
+    </>
+  )
 }
