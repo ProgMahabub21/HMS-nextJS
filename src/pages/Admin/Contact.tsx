abbbf16d42c9f6b1b71d341d339ffae6a31a1125
S A Saharukh <sasaharukh@gmail.com>
2023-04-07 19:39:16 +0600

    working with layout

---
+++ src/pages/Admin/Contact.tsx
@@ -0,0 +1,17 @@
+import MainLayout from "./layout/MainLayout";
+
+
+const Contact = () => {
+    return (
+        <MainLayout>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div className="px-4 py-6 sm:px-0">
+                    <h1 className="text-3xl font-bold text-gray-900">Contact Us</h1>
+                    <p className="mt-2 text-lg text-gray-600">You can reach us at contact@myapp.com.</p>
+                </div>
+            </div>
+        </MainLayout>
+    );
+};
+
+export default Contact;
