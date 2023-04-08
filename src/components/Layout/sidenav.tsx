e17c92583747c3e8f42849d3078638c4ceee2e38
S A Saharukh <sasaharukh@gmail.com>
2023-04-08 17:20:05 +0600

    login error added

--- src/components/Layout/sidenav.tsx
+++ src/components/Layout/sidenav.tsx
@@ -16,7 +16,7 @@ const Sidenav = ({ children }: Props) => {
 
     let navBarItems: {
         name: string;
-        link: string;
+        link?: string;
         icon: JSX.Element;
         children?: {
             name: string;
@@ -26,23 +26,23 @@ const Sidenav = ({ children }: Props) => {
 
     console.log("currentPageName ", currentPageName);
 
-    switch (currentPageName) {
-        case "Admin":
+    switch (currentPageName.toLocaleLowerCase()) {
+        case "admin":
             navBarItems = [
                 {
                     name: "Dashboard",
-                    link: "/Admin/dashboard",
+                    link: "/admin/dashboard",
                     icon: <FaFlipboard />,
 
                 },
                 {
                     name: "Patients",
-                    link: "/Admin/patients",
+
                     icon: <FaFlipboard />,
                     children: [
                         {
                             name: "Patient List",
-                            link: "/Admin/patients/add-patient",
+                            link: "/admin/patients",
                         },
                         {
                             name: "Add Patient",
@@ -87,7 +87,7 @@ const Sidenav = ({ children }: Props) => {
                 >
                     <path
                         clipRule="evenodd"
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
                     ></path>
                 </svg>
@@ -129,7 +129,7 @@ const Sidenav = ({ children }: Props) => {
                                 >
                                     <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
                                     <path
-                                        fill-rule="evenodd"
+                                        fillRule="evenodd"
                                         d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
                                         clipRule="evenodd"
                                     ></path>
@@ -169,7 +169,7 @@ const Sidenav = ({ children }: Props) => {
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <path
-                                        fill-rule="evenodd"
+                                        fillRule="evenodd"
                                         d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z"
                                         clipRule="evenodd"
                                     ></path>
