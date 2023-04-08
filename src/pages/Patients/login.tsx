b97f938d9bc5bee75ca209bc675241523cf1498b
ProMahabub21 <imahbub.1999@gmail.com>
2023-04-08 21:54:20 +0600

    a

--- src/pages/Patients/login.tsx
+++ src/pages/Patients/login.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import { SubmitHandler } from 'react-hook-form';
 import  lgimage from '/public/image/login-p.jpg'
+import axios, {isCancel, AxiosError, AxiosResponse} from 'axios';
 //import { redirect } from 'next/router';
 
 type LoginFormData = {
@@ -10,8 +11,43 @@ type LoginFormData = {
     password: string;
   };
 
-const onSubmit = (data: LoginFormData) => {
-    console.log(data);
+const onSubmit = async (data: LoginFormData) => {
+    
+
+    //json stringify
+    const jsondata = JSON.stringify(data);
+
+    console.log(jsondata);
+
+    // call api and fetch data from " http://localhost:3005/patients/finduser" and match email password
+
+    // if match redirect to "http://localhost:3005/Patients/homepage"
+
+    // if not match show error message
+    const axios = require("axios");
+    const response = await axios.post("http://localhost:3000/patients/login", jsondata)
+    
+    .then((response: AxiosResponse) => {
+
+      console.log(response)
+      // const fetcheddata:any = response.data.email;
+      // console.log(fetcheddata);
+      // // Check if the API response indicates a match
+      // if (data.email == fetcheddata.email && data.password == fetcheddata.password) {
+      //   // Redirect to homepage
+      //   window.location.href = "http://localhost:3000/Patients/homepage";
+      // } else {
+      //   // Show error message
+      //   console.error("Email and password do not match.");
+      //   // You can display the error message in your desired way, e.g., by appending it to the DOM or showing an alert.
+      // }
+    })
+    .catch((error: AxiosError) => {
+      console.error("Failed to fetch data from API:", error);
+      // Handle error, e.g., by showing an error message
+    });
+
+    
 };  
 
 const LoginPage = () => {
