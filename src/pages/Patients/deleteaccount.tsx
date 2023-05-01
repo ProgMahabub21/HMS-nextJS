cfdbd4fdfba873ca5eb7868d7e09748eddb1174a
ProMahabub21 <imahbub.1999@gmail.com>
2023-05-02 02:38:17 +0600

    post, delete, and css fixed

--- src/pages/Patients/deleteaccount.tsx
+++ src/pages/Patients/deleteaccount.tsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosInstance } from '../common/axios';
+import { useRouter } from 'next/router';
+import Sidebar from "./Components/sidebar";
+export default function DeleteAccount() {
 
-export default function DeleteAccount () {
+
+    const [UUID, setUUID] = useState('');
+    const router = useRouter();
+    
+
+    const handleUUIDChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUUID(e.target.value);
+    };
+    
+    const action = async (e: React.FormEvent<HTMLFormElement>) => {
+        try {
+            // delete http response
+            e.preventDefault();
+           const response = await axiosInstance.delete(`patients/deletepatients/${UUID}`);
+
+            if (response.status === 200) {
+                console.log("Account deleted successfully.");
+                // display a message to the user
+                alert("Account deleted successfully.");
+            }
+
+            router.push("/Patients/login");
+        }catch (error: any) {
+            console.log(error.data.message);
+        }
+
+    }
+
+
     return (
         <>
-            <h1>Delete Account</h1>
+            <div className="grid grid-cols-12 ">
+
+                <Sidebar />
+                <div className="flex flex-col items-center justify-center col-span-9 space-y-4 md:w-1/2">
+                    <div className="container flex flex-col min-h-screen mx-auto">
+                        <div className="p-8 px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded item-center" role="alert">
+                            <strong className="font-bold">Alert!</strong>
+                        
+                            <span className="block sm:inline">You are about to delete your account!! </span>
+                            <span className="block sm:inline"> If you proceed , it will erase all data associated with the account</span>
+                        </div>
+                        <form onSubmit={action} className="max-w-sm p-8 rounded-lg shadow-md ">
+                            <div className="mb-6">
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter Your Account UUID</label>
+                                <input type="UUID" id="email" onChange={handleUUIDChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+                                 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" required />
+                            </div>
+                            
+                            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </form>
+                    </div></div>
+
+            </div>
         </>
     )
 }
--- src/pages/Patients/login.tsx
+++ src/pages/Patients/login.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import { SubmitHandler } from 'react-hook-form';
@@ -6,6 +6,7 @@ import lgimage from '/public/image/login-p.jpg'
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 import { axiosInstance } from '../common/axios';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 //import { redirect } from 'next/router';
 
 type LoginFormData = {
@@ -23,7 +24,7 @@ const LoginPage = () => {
     //json stringify
     const email = data.email;
     const password = data.password;
-    console.log (email , password)
+    console.log(email, password)
 
     // call api and fetch data from " http://localhost:3005/patients/finduser" and match email password
 
@@ -40,7 +41,8 @@ const LoginPage = () => {
 
     } catch (error: any) {
       console.log(error);
-      setErrors(error.response.data.message);
+      if (error.response.status == 500)
+        setErrors(error.response.status + " login Credential invalid");
 
     }
 
@@ -60,13 +62,13 @@ const LoginPage = () => {
           className='object-cover h-full'
         />
       </div>
-      <div className="flex items-center justify-center bg-gray-100 md:w-1/2">
-     { errors && <div className="relative px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded" role="alert">
-                    <strong className="font-bold">Error!</strong>
-                    <span className="block sm:inline">{errors}</span>
-                </div> }
-        <form
-          className="max-w-sm p-8 bg-white rounded-lg shadow-md"
+      <div className="flex flex-col items-center justify-center space-y-4 bg-gray-100 md:w-1/2">
+        {errors && <div className="p-8 px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded item-center" role="alert">
+          <strong className="font-bold">Error!</strong>
+          <span className="block sm:inline">{errors}</span>
+        </div>}
+        <div><form
+          className="max-w-sm p-8 bg-white rounded-lg shadow-md "
           onSubmit={handleSubmit(onSubmit)}
         >
           <h2 className="mb-4 text-2xl font-bold">Log in to your account</h2>
@@ -98,7 +100,11 @@ const LoginPage = () => {
           >
             Log in
           </button>
-        </form>
+          <p className="mt-5 text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <label onClick={() => router.push("/Patients/registration")} className="font-medium text-blue-400 text-primary-600 hover:underline dark:text-primary-500">Sign up</label>
+          </p>
+        </form></div>
+
       </div>
     </div>
   );
