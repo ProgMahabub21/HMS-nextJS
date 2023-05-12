6a78efacb2f99fc23997dd56eff3b632dfe93499
S A Saharukh <sasaharukh@gmail.com>
2023-05-12 15:16:24 +0600

    stripe

--- src/pages/Admin/dashboard.tsx
+++ src/pages/Admin/dashboard.tsx
@@ -7,6 +7,7 @@ export default function Dashboard() {
             <div className="grid grid-cols-12 ">
                 <div className="col-span-9">
                     <h1 className="h1">Dashboard</h1>
+                    <h1 className="h1">Dashboard</h1>
                 </div>
             </div>
 
