9ede39310bd1e09097fd886df759e9fd8bd09458
S A Saharukh <sasaharukh@gmail.com>
2023-05-15 00:47:06 +0600

    live url

--- src/common/axios/index.ts
+++ src/common/axios/index.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 
-  //  baseURL: 'https://medgrid.up.railway.app/', // live
+   baseURL: 'https://medgrid.up.railway.app/', // live
 
 
- baseURL: 'http://localhost:3000/',
+//  baseURL: 'http://localhost:3000/',
   // timeout: 1000,
   headers: { 'Content-Type': 'application/json'}
   , withCredentials: true,
